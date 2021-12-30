5a0755f878bfcd64f905f618cce5d81e3e2f9780
leiyang <12345678>
2021-12-30 14:51:45 +0800

    简化日志打印

--- checkin.py
+++ checkin.py
@@ -1,16 +1,18 @@
+# coding=utf-8
 import sys
 import requests
 import argparse
 import time
+import json
 
 
 if __name__ =='__main__':
     print("argv:[%s]" % str(sys.argv))
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--cookie", help='''
-                                                请到【https://glados.rocks/console/checkin】页面拷贝cookie
-                                                注意：一定要清除掉cookie值之间的空格，不然无法识别!!!
-                                                ''')
+                                               请到【https://glados.rocks/console/checkin】页面拷贝cookie
+                                               注意：一定要清除掉cookie值之间的空格，不然无法识别!!!
+                                               ''')
     cookie = parser.parse_args().cookie
     if(cookie != None):
         print("解析到的Cookie:[%s]" % cookie)
@@ -22,7 +24,8 @@ if __name__ =='__main__':
             result = requests.post(url='https://glados.rocks/api/user/checkin', data={"token": "glados_network"},
                                    headers=headers).text
             nowTime = time.strftime('%Y-%m-%d %X', time.localtime())
-            print("time:[%s]  ,response:[%s]" % (nowTime, result))
+            result_json = json.loads(result)
+            print("time:[%s]  ,response:[%s]" % (nowTime, result_json['message']))
             # 休眠一天
             time.sleep(60 * 60 * 24)
         else:
