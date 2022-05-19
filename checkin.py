eb3a89be915ae5bab9e6f69b5d1ef9376c969494
leiyang <12345678>
2022-05-19 16:02:23 +0800

    修正参数

--- checkin.py
+++ checkin.py
@@ -11,41 +11,45 @@ logging.basicConfig(format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(leve
                     level=logging.DEBUG)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--cookie", help='''
-                                                请到【https://glados.rocks/console/checkin】页面拷贝cookie
-                                                注意：一定要清除掉cookie值之间的空格，不然无法识别!!!
-                                                ''')
-    if parser is not None and parser.parse_args().cookie is not None:
-        cookie = parser.parse_args().cookie
-
-    else:
-        env_dist = os.environ
-        cookie = env_dist.get('cookie')
-
-    if cookie is not None:
-        cookie = str(cookie).replace(" ", "")
-        logging.info("解析到的cookie:[%s]" % cookie)
-    else:
-        logging.info("未解析到cookie,请检查启动参数!")
-        sys.exit(0)
-
-    while True:
-        if cookie is not None:
-            headers = {'Cookie': str(cookie)}
-            result = requests.post(url='https://glados.rocks/api/user/checkin', data={"token": "glados.network"},
-                                   headers=headers).text
-            nowTime = time.strftime('%Y-%m-%d %X', time.localtime())
-            result_json = json.loads(result)
-            # print("time:[%s]  ,response:[%s]" % (nowTime, result_json['message']))
-            logging.info("time:[%s]  ,response:[%s]" % (nowTime, result_json['message']))
-            # 休眠一天
-            time.sleep(60 * 60 * 24)
+    try:
+        parser = argparse.ArgumentParser()
+        # 优先获取命令行参数
+        if parser is not None and parser.parse_args() is not None and parser.parse_args().cookie is not None:
+            cookie = parser.parse_args().cookie
         else:
-            logging.info('''
-                    请设置cookie运行,例如：
-                    docker run -itd glados-checkin:1.0.0 --name my-glados-checkin -c='value'
-                    请到【https://glados.rocks/console/checkin】页面拷贝cookie
-                    注意：一定要清除掉cookie值之间的空格，不然无法识别!!!
-                    ''')
-            break
+            # 获取系统变量
+            env_dist = os.environ
+            cookie = env_dist.get('cookie')
+
+        if cookie is not None:
+            cookie = str(cookie).replace(" ", "")
+            logging.info("解析到的cookie:[%s]" % cookie)
+        else:
+            logging.info("未解析到cookie,请检查启动参数!")
+            sys.exit(0)
+
+        while True:
+            if cookie is not None:
+                try:
+                    headers = {'Cookie': str(cookie)}
+                    result = requests.post(url='https://glados.rocks/api/user/checkin', data={"token": "glados.network"}
+                                           , headers=headers).text
+                    nowTime = time.strftime('%Y-%m-%d %X', time.localtime())
+                    result_json = json.loads(result)
+                    # print("time:[%s]  ,response:[%s]" % (nowTime, result_json['message']))
+                    logging.info("time:[%s]  ,response:[%s]" % (nowTime, result_json['message']))
+                    # 休眠一天
+                    time.sleep(60 * 60 * 24)
+                except Exception as ex:
+                    logging.error("程序出现异常!" + str(ex))
+                    logging.error("请到【https://glados.rocks/console/checkin】页面拷贝cookie,注意：一定要清除掉cookie值之间的空格，不然无法识别!!!")
+            else:
+                logging.info('''
+                        请设置cookie运行,例如：
+                        docker run -itd glados-checkin:1.0.0 --name my-glados-checkin -c='value'
+                        请到【https://glados.rocks/console/checkin】页面拷贝cookie
+                        注意：一定要清除掉cookie值之间的空格，不然无法识别!!!
+                        ''')
+                break
+    except Exception as ex:
+        logging.error("程序出现异常!" + str(ex))
