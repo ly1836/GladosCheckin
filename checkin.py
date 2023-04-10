a16edb0332d2465be1cf54fd8a156bf05a526af0
leiyang <12345678>
2023-04-10 16:04:45 +0800

    1.增加cookie.json文件示例 2.增加http_proxy,https_proxy代理参数

--- checkin.py
+++ checkin.py
@@ -11,29 +11,50 @@ import random
 logging.basicConfig(format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s',
                     level=logging.DEBUG)
 
-def get_cookies():
-    cookies = set()
-    # 优先获取环境变量
+proxies = {}
+cookies = set()
+
+'''
+    读取配置
+'''
+def read_configuration():
+    global proxies
+    global cookies
+    http_proxy = None
+    https_proxy = None
+
+    # step1.优先获取环境变量
     env_dist = os.environ
     # step1.获取系统环境变量中的
     try:
         for key in env_dist.keys():
-            if "cookie_" in key:
-                cookie = env_dist.get(key)
-                if cookie is not None:
-                    cookies.add(str(cookie))
-    except Exception as ex:
-        print(ex)
+            if "http_proxy" in key:
+                http_proxy = env_dist.get(key)
 
-    # step2.获取指定路径下json文件,/config/cookies.json
+            if "https_proxy" in key:
+                https_proxy = env_dist.get(key)
+
+            if "cookie_" in key:
+                if env_dist.get(key) is not None:
+                    cookies.add(str(cookie))
+
+    except Exception as e:
+        print(e)
+
+    # step2.获取指定路径下json文件【/config/cookies.json】
     try:
-        with open("/config/cookies.json", 'r') as load_f:
+        with open("./11cookies.json", 'r') as load_f:
             load_dict = json.load(load_f)
             print(load_dict)
-            for c in load_dict:
+
+            # 代理
+            http_proxy = load_dict.get('http_proxy', "")
+            https_proxy = load_dict.get('https_proxy', "")
+
+            for c in load_dict['cookies']:
                 cookies.add(str(c['cookie']))
-    except Exception as ex:
-        print(ex)
+    except Exception as e:
+        print(e)
 
     # step3.获取命令行的
     if len(cookies) == 0:
@@ -43,15 +64,22 @@ def get_cookies():
                                                                 请到【https://glados.rocks/console/checkin】页面拷贝cookie
                                                                 注意：一定要清除掉cookie值之间的空格，不然无法识别!!!
                                                                 ''')
-        cookie = parser.parse_args().cookie
-        cookies.add(cookie)
+        parser.add_argument("-http_proxy", "--http_proxy", help='http代理')
+        parser.add_argument("-https_proxy", "--https_proxy", help='https代理')
+        http_proxy = parser.parse_args().http_proxy
+        https_proxy = parser.parse_args().https_proxy
+        cookies.add(parser.parse_args().cookie)
 
-    return cookies
+    if http_proxy is not None:
+        proxies['http'] = str(http_proxy)
+    if http_proxy is not None:
+        proxies['https'] = str(https_proxy)
+
 
 if __name__ == '__main__':
-    try:
-        cookies = get_cookies()
+    read_configuration()
 
+    try:
         if cookies is not None:
             for cookie in cookies:
                 cookie = str(cookie).replace(" ", "")
@@ -66,9 +94,11 @@ if __name__ == '__main__':
                 try:
                     for cookie in cookies:
                         headers = {'Cookie': str(cookie)}
+
                         result = requests.post(url='https://glados.rocks/api/user/checkin',
                                                data={"token": "glados.network"},
-                                               headers=headers).text
+                                               headers=headers,
+                                               proxies=proxies).text
                         nowTime = time.strftime('%Y-%m-%d %X', time.localtime())
                         result_json = json.loads(result)
                         # print("time:[%s]  ,response:[%s]" % (nowTime, result_json['message']))
